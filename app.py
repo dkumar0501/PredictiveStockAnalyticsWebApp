bbc5660c42ed60c3d9f085a0c876a0781f66934d
D Kumar <deepkumar0501@gmail.com>
2025-07-10 12:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np  
 import pandas as pd
 import matplotlib.pyplot as plt 
-import yfinance as yf
+import yfinance as yf 
 from keras.models import load_model  
 import streamlit as st
 
