22025699506e3075a3d2a9ef342f28e01a789e64
D Kumar <deepkumar0501@gmail.com>
2025-07-27 14:38:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf   
 from keras.models import load_model   
 import streamlit as st   
-     
+      
 # Set date range   
 start = '2010-01-01'  
 end = '2024-12-31'  
