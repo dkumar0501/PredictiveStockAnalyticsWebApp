95c2f661e86a3517cf890d0f027b7246785cff31
D Kumar <deepkumar0501@gmail.com>
2025-09-04 23:45:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt         
 import yfinance as yf     
 from keras.models import load_model       
-import streamlit as st     
+import streamlit as st      
                
 # Set date range        
 start = '2010-01-01'             
